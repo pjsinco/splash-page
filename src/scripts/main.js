d2157a3e34425d3b4222127d9d0bd04476111fa5
pjsinco <psinco@gmail.com>
2018-03-28 19:27:37 -0500

    Implement first version of the fixed first feature

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
-require('./../../node_modules/waypoints/lib/noframework.waypoints.min.js')
+//require('./../../node_modules/waypoints/lib/noframework.waypoints.min.js')
 
-window.onload =function() {
+$(document).ready(function() {
+
   const reveals = document.querySelectorAll('.reveal')
+  const firstFeature = document.querySelector('#firstFeature')
 
   reveals.forEach(function(revealElement) {
-    const waypoint = new Waypoint({
+    new Waypoint({
       element: revealElement,
       handler: function() {
         this.element.classList.add('on')
@@ -13,13 +15,49 @@ window.onload =function() {
     })
   })
 
-  const waypoint = new Waypoint({
+  const fan = new Waypoint({
     element: document.querySelector('.fan'),
     handler: function(direction) {
       this.element.classList.add('fan-out')
     },
-    offset: '25%',
+    offset: '-25%',
   })
 
-}
+  new Waypoint({
+    element: document.querySelector('.super'),
+    handler: function(direction) {
+      if (direction === 'down') {
+        firstFeature.classList.remove('fixed')
+        firstFeature.style.paddingTop = '50px'
+        firstFeature.scrollIntoView()
+      } 
+    },
+    offset: '-100%',
+  })
+
+  new Waypoint({
+    element: document.querySelector('#fixedFlag'),
+    handler: function(direction) {
+      document.querySelector('.super').style.marginBottom
+      if (direction === 'up') {
+        firstFeature.classList.add('fixed') 
+        firstFeature.style.paddingTop = '0'
+        //window.scrollBy(0, -window.innerHeight) 
+        window.scrollBy(0, -856) 
+      }
+    },
+    //offset: '50px',
+  })
+
+//  const fixedItemWaypoint = new Waypoint({
+//    element: fixedItem,
+//    handler: function(direction) {
+//console.log(direction);
+//      if (direction === 'up') {
+//console.log('fixeditemgoingup');
+//        this.element.classList.add('fixed')
+//      }
+//    }
+//  })
+})
 
